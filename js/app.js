e2de6abda91715849590e79b8224ad93bfdbce67
victorialung <vtr.lung@outlook.es>
2017-11-20 04:23:35 -0500

    terminada version 0.0.3

--- js/app.js
+++ js/app.js
@@ -2,17 +2,38 @@ window.addEventListener('load', function() {
   var btnTweet = document.getElementById('btn-tweet');
   var inputTweet = document.getElementById('input-tweet');
   var containerTweets = document.getElementById('container-tweets');
-  
+  var countLetter = document.getElementById('count-letters');
+
   btnTweet.addEventListener('click', function(event) {
     if (inputTweet.value) {
-      var parentDiv = document.createElement('div');
       var contentTweet = document.createElement('p');
+      contentTweet.className = 'content-tweet';
       contentTweet.innerHTML = inputTweet.value;
-      parentDiv.appendChild(contentTweet);
-      containerTweets.appendChild(parentDiv);
+      containerTweets.appendChild(contentTweet);
+      containerTweets.style.display = 'block';
       inputTweet.value = '';
     } else {
       btnTweet.disabled = true;
+    }
+  });
+
+  inputTweet.addEventListener('keypress', function(event) {
+    var countingLetters = inputTweet.value.length;
+    var maxLetters = 139;
+    countLetter.style.display = 'inline-block';
+    countLetter.innerHTML = maxLetters - countingLetters;
+    if (countingLetters >= 0 && countingLetters <= 120) {
+      btnTweet.disabled = false;
+      document.getElementById('count-letters').setAttribute('class', 'first-color');
+    } else if (countingLetters >= 120 && countingLetters <= 130) {
+      btnTweet.disabled = false;
+      document.getElementById('count-letters').setAttribute('class', 'sec-color');
+    } else if (countingLetters >= 130 && countingLetters < 140) {
+      btnTweet.disabled = false;
+      document.getElementById('count-letters').setAttribute('class', 'third-color');
+    } else if (countingLetters >= 140) {
+      btnTweet.disabled = true;
+      document.getElementById('count-letters').setAttribute('class', 'third-color');
     }
   });
 });
